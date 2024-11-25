df980e07e835521364ced2507534e9da8d24d4cf
Gmarcarini <gmarcarini.silveira@gmail.com>
2024-11-25 13:37:38 -0300

    atualizando jogo.py

--- jogo.py
+++ jogo.py
@@ -1,53 +1,60 @@
 from mestre import Mestre
+mestre = Mestre()
 
 print('Adventure of Rusty Axe Tavern')
 print('Bem Vindo jogador')
 print('Escolha o se personagem')
-print('Borin o Bárbaro')
+print('1. Borin o Bárbaro')
+print('2. Mirah a Barda')
+print('3. Osric o Clérigo')
+print('4. Fenric o Druida')
+print('5. Lorik o Guerreiro')
+print('6. Silas o Ladino')
+print('7. Cyria a Maga')
+print('8. Kyan o monge')
+print('9. Edras o paladino')
+print('10. Korvin o patrulheiro')
+print('11. Zethrin o Feiticero')
+print('12. Drazel o Bruxo')
 
 
 
-mestre = Mestre()
 mensagem = mestre.mensagem('Começar Aventura')
 print(mensagem)
+
+def dialogo(npc, acao):
+    resposta_ia = Mestre.mensagem(f'O jogador começa um dialogo com o {npc}: {acao}')
+    while resposta_ia != 'Dialogo encerrado':
+        print(resposta_ia)
+        acao = input(f'nome do personagem do jogador: ')
+        resposta_ia = Mestre.mensagem(f'Ação do jogador: {acao}')
+
+def inventario():
+    resposta_ia = Mestre.mensagem('O jogador quer verificar oq ele tem dentro da sua mochila: Responda com uma lista de todos os itens que o jogador tem')
+    print(resposta_ia)
+    acao = input('')
+    return  acao
+
+
+def combate(inimigos):
+    contador = 1
+    resosta_ia = Mestre.mensagem(f'O jogador inicia o combate com {inimigos}:')
+    print(resosta_ia)
+    while combate == True:
+        print(f'Rodada {contador}')
+        acao = input('')
+        resposta_ia =  resosta_ia = Mestre.mensagem(f'Ação do jogador: {acao}')
+        print(resposta_ia)
+        contador += 1
+
+
+
+
+
+
 continuar = True
 
 while continuar != False:
     acao = input('')
     mensagem = mestre.mensagem(acao)
     print(mensagem)
-
-
-
-
-'''from mestre import Syntheris, Lorealis, Naratheon, Veridex
-
-
-syntheris = Syntheris()
-naratheon = Naratheon()
-veridex = Veridex()
-lorealis = Lorealis()
-
-resposta_syntheris = syntheris.chat('começar')
-acao_jogador = ''
-
-while acao_jogador != 'sair do jogo':
-    if resposta_syntheris == '1':
-        prompt_lorealis = syntheris.mensagem('O que você deseja que a Lorealis faça:')
-        resposta_lorealis = lorealis.mensagem(prompt_lorealis)
-        syntheris.mensagem(f'Lorealis: {resposta_lorealis}')
-
-    elif resposta_syntheris == '2':
-        prompt_naratheon = syntheris.mensagem('O que você deseja que a Naratheon faça:')
-        resposta_naratheon = naratheon.mensagem(prompt_naratheon)
-        print(resposta_naratheon)
-        acao_jogador = input('')
-        syntheris.mensagem(f'Naratheon: {resposta_naratheon}\nJogador: {acao_jogador}')
-
-    elif resposta_syntheris == '3':
-        prompt_veridex = syntheris.mensagem('O que você deseja que a Veridex faça:')
-        resposta_veridex = veridex.mensagem(prompt_veridex)
-        syntheris.mensagem(f'Veridex: {resposta_veridex}')
-
-    else:
-        prompt_syntheris = syntheris.mensagem("lembre-se da ordem de atuação, primeiro voce responde somente somente com o número da IA que quer coordenar, depois que receber a mensagem 'O que você deseja que a IA faça:' Voce escreve o prompt com as informações")'''
